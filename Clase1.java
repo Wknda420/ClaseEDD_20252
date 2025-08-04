ce020d326e6d764e4fcb412578a6f8e1c3ebf6c6
Wanda <wanda.montanobet@campusucc.edu.co>
2025-08-04 14:03:02 +0000

    detalle

--- Clase1.java
+++ Clase1.java
@@ -2,6 +2,23 @@ public class Clase1 {
     public static void main (String []args){
 
         System.out.println("Estructura de datos");
+
+        //arreglos o arrays o vectores
+
+        //declaracion del arreglo. Forma 1
+
+        //int[] numeros;
+        //numeros = new int[5];
+
+
+        //Declaracion del arrelgo. Forma 2
+
+        //int[] numeros = new int[5];
+
+
+        //Declaracion e inicializacion
         
+        int[] numeros = {10, 20, 30 ,40 50};
+
     }
 }
