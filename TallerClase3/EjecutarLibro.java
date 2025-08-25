99bdd668a34ebddc21d43b7fbfa7b000a629fe1e
Wanda <wanda.montanobet@campusucc.edu.co>
2025-08-25 15:06:09 +0000

    llaves

--- TallerClase3/EjecutarLibro.java
+++ TallerClase3/EjecutarLibro.java
@@ -2,8 +2,10 @@ package TallerClase3;
 
 public class EjecutarLibro {
     public static void main(String[] args) {
-        Libro[] biblioteca = new Libro[]{
-            new Libro("Poesia Completa", "Alejandra Pizarnik", 43.000)
-        };
+        Libro[] biblioteca = new Libro[6];
+            new Libro("Poesia Completa", "Alejandra Pizarnik", 43000);
+            new Libro("Cien años de soledad", "Gabriel Garcia Marquez", 35000);
+            new Libro("Cronicas de una muerte anunciada", "Gabriel Garcia Marquez", 38000);
+        
     }
 }
