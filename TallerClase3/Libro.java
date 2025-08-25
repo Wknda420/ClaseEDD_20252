84b02d14ae96ec4448a8da153f66e59cc7b2e515
Wanda <wanda.montanobet@campusucc.edu.co>
2025-08-25 14:30:29 +0000

    libro

---
+++ TallerClase3/Libro.java
@@ -0,0 +1,27 @@
+package TallerClase3;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private double precio;   
+} 
+    public String getTitulo() {
+            return titulo;
+        }
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+        public String getAutor() {
+            return autor;
+        }
+        public void setAutor(String autor) {
+            this.autor = autor;
+        }
+        public double getPrecio() {
+            return precio;
+        }
+        public void setPrecio(double precio) {
+            this.precio = precio;
+        }
+    
+
