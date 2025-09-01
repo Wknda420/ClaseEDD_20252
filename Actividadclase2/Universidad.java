5d717c4f404c555c2d047a10da1296f3c12352d3
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-01 04:45:14 +0000

    creacion de estudiantes ejemplo y los metodods de  estadistica

--- Actividadclase2/Universidad.java
+++ Actividadclase2/Universidad.java
@@ -2,4 +2,11 @@ package Actividadclase2;
 
 public class Universidad {
     private static List<Estudiante> estudiantes = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String[] cursos1 = {"Matematicas", "Fisica", "Programacion"};
+        String[] cursos2 = {"Electricidad", "Literatura"};
+        String[] cursos3 = {"Biologia", "Quimica", "Anatomia"};
+
+    }
 }
