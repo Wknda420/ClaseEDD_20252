fc39addc1606cc0cb3e48953ed1663df71ab39a7
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-01 20:55:21 +0000

    correciones en el main

--- TallerClase3/EjecutarLibro.java
+++ TallerClase3/EjecutarLibro.java
@@ -9,18 +9,17 @@ public class EjecutarLibro {
             Arena [3] = new Libro("Harry Potter y la piedra filosofal", "J.K Rowwling", 50000);
             Arena [4] = new Libro("Don Quijote de la Mancha", "Miguel de Cervantes", 60000);
             Arena [5] = new Libro("El señor de los anillos", "J.R.R Tolkien", 65000);
-        }
 
-        System.out.println("|====Libros Y Precios====");
+            System.out.println("=== LIBROS Y PRECIOS ===");
         for (Libro libro : Arena) {
             libro.mostrarInformacion();
         }
-
+        
         double total = 0;
         for (Libro libro : Arena) {
             total += libro.getPrecio();
         }
-
-        System.out.println("Precio total: $" + String.format("%,.0f", total) + "COP");
+        
+        System.out.println("Precio total: $" + String.format("%,.0f", total) + " COP");
     }
-}     
+}
