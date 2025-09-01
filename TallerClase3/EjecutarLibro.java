5ad5f6be7a4dbc1559a010a2fd6559d5bdff3753
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-01 20:55:53 +0000

    .

--- TallerClase3/EjecutarLibro.java
+++ TallerClase3/EjecutarLibro.java
@@ -6,7 +6,7 @@ public class EjecutarLibro {
             Arena [0] = new Libro("Poesia Completa", "Alejandra Pizarnik", 43000);
             Arena [1] = new Libro("Cien años de soledad", "Gabriel Garcia Marquez", 35000);
             Arena [2] = new Libro("Cronicas de una muerte anunciada", "Gabriel Garcia Marquez", 38000);
-            Arena [3] = new Libro("Harry Potter y la piedra filosofal", "J.K Rowwling", 50000);
+            Arena [3] = new Libro("Harry Potter y la piedra filosofal", "J.K Rowling", 50000);
             Arena [4] = new Libro("Don Quijote de la Mancha", "Miguel de Cervantes", 60000);
             Arena [5] = new Libro("El señor de los anillos", "J.R.R Tolkien", 65000);
 
