2bcceb92a805c618546f50d44838b89bf367f2d0
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-01 04:35:15 +0000

    para id del estudiante

--- Actividadclase2/Estudiante.java
+++ Actividadclase2/Estudiante.java
@@ -3,5 +3,10 @@ package Actividadclase2;
 public class Estudiante {
     private int id; //id del estudiante
     private String[] cursos; //array con nombres de los cursos
+
+    public Estudiante(int id, String[] Cursos) {
+        this.id = id;
+        this.cursos = cursos;
+    }
 }
 
