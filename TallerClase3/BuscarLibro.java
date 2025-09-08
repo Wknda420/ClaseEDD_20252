5fc166c9320a9ac124ebc2c48a92e77fa99e4176
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-08 05:29:22 +0000

    buscar y terminar busqueda

--- TallerClase3/BuscarLibro.java
+++ TallerClase3/BuscarLibro.java
@@ -7,6 +7,21 @@ public class BuscarLibro {
         misLibros[0] = new Libro("Cien años de soledad", "Garcia Marquez", 35000);
         misLibros[1] = new Libro("Harry Potter", "J.K Rowling", 50000);
         misLibros[2] = new Libro("Don Quijote", "Cervantes", 60000);
-        
+
+        String libroBuscado = "Harry Potter";
+        boolean encontrado = false;
+
+        for (Libro libro: misLibros) {
+            if (libro.getTitulo().equals(libroBuscado)) {
+                System.out.println("¡Libro encontrado!");
+                libro.mostrarInformacion();
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("El libro '" + libroBuscado + "' no se encuentra");
+        }
     }
 }
