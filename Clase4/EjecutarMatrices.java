fd7ddf66227f1382f0e1da84dba2ca55e0a4adde
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-08 09:13:13 +0000

    .

---
+++ Clase4/EjecutarMatrices.java
@@ -0,0 +1,13 @@
+package Clase4;
+
+public class EjecutarMatrices {
+    public static void main(String[] args) {
+        
+        MatricesEstructuraDeDatos objMatrices = new MatricesEstructuraDeDatos();
+
+        int[][] m = objMatrices.llenarMatrizEnteros(3, 3);
+
+        System.out.println(objMatrices.mostrarMatrizEnteros(m));
+
+    }
+}
