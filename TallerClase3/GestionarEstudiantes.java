fbc8c0cdc321ea993785b51c9885c95e037e90a5
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-08 05:42:45 +0000

    arreglo

--- TallerClase3/GestionarEstudiantes.java
+++ TallerClase3/GestionarEstudiantes.java
@@ -2,6 +2,10 @@ package TallerClase3;
 
 public class GestionarEstudiantes {
     public static void main(String[] args) {
+        Estudiante[] estudiantes = new Estudiante[5];
+        int totalEstudiantes = 0;
+
+
         
     }
 }
