d75882b16453d77de934ed8ee16c7ba9270c0cb2
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-08 05:35:01 +0000

    constructor

--- TallerClase3/Estudiante.java
+++ TallerClase3/Estudiante.java
@@ -4,4 +4,36 @@ public class Estudiante {
     private String nombre;
     private String matricula;
     private double promedio;
+
+    public Estudiante (String nombre, String matricula, double promedio) {
+        this.nombre = nombre;
+        this.matricula = matricula;
+        this.promedio = promedio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    
 }
