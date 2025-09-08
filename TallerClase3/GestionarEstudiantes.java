34e56c7439e2a63a01c88069fb2e13c451057188
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-08 05:43:21 +0000

    estudiantes

--- TallerClase3/GestionarEstudiantes.java
+++ TallerClase3/GestionarEstudiantes.java
@@ -6,6 +6,13 @@ public class GestionarEstudiantes {
         int totalEstudiantes = 0;
 
 
+        estudiantes[totalEstudiantes] = new Estudiante("Ana", "2023001", 4.5);
+        totalEstudiantes++;
         
+        estudiantes[totalEstudiantes] = new Estudiante("Carlos", "2023002", 3.8);
+        totalEstudiantes++;
+        
+        estudiantes[totalEstudiantes] = new Estudiante("Maria", "2023003", 4.2);
+        totalEstudiantes++;
     }
 }
