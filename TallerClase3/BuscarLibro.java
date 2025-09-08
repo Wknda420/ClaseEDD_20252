15cb1466df3e95757d03d77c3f67f3febda2722b
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-08 05:22:45 +0000

    clase buscar

---
+++ TallerClase3/BuscarLibro.java
@@ -0,0 +1,12 @@
+package TallerClase3;
+
+public class BuscarLibro {
+    public static void main(String[] args) {
+        Libro[] misLibros = new Libro[3];
+        
+        misLibros[0] = new Libro("Cien años de soledad", "Garcia Marquez", 35000);
+        misLibros[1] = new Libro("Harry Potter", "J.K Rowling", 50000);
+        misLibros[2] = new Libro("Don Quijote", "Cervantes", 60000);
+        
+    }
+}
