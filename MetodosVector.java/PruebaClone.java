aed3ed04b89a55e1681a1c8d07d3f557f1c8f8ef
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-22 05:29:37 +0000

    clone

---
+++ MetodosVector.java/PruebaClone.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class PruebaClone {
+    public static void main(String[] args) {
+        Stack<String> Original = new Stack<>();
+
+        Original.push("Gato Siames");
+        Original.push("Gato Persa");
+        Original.push("Gato Carey");
+
+        System.out.println("Pila original: " + Original);
+
+        Stack<String> clone = (Stack<String>) Original.clone();
+
+        System.out.println("Copia: " + clone);
+        
+
+    }
+}
