c32be1bf2f49814b0ee0498c4c5e8817e0d954a0
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-22 14:25:04 +0000

    metodos

--- clase8/ColasU1.java
+++ clase8/ColasU1.java
@@ -7,6 +7,10 @@ public class ColasU1 {
         
         Queue<Integer> cola = new ArrayDeque<>();
 
+        System.out.println("                       ");
+        System.out.println(cola.peek()); //aqui deberia retornar null
+        System.out.println("--------");
+
         cola.add(5);
         cola.add(10);
         cola.add(3);
@@ -21,7 +25,33 @@ public class ColasU1 {
         System.out.println(cola.element());
         System.out.println("                  ");
 
-        
+        //offer - agregar un elemento retorna un true
+        System.out.println(cola.offer(9));
+        System.out.println(cola);
+        System.out.println("               ");
+
+        System.out.println(cola.peek());
+        System.out.println("--------------------");
+
+        //poll - devuelve null si esta vacia, remueve un elemento
+        System.out.println("Remueve el elemento: ");
+        System.out.println(cola.poll());
+        System.out.println("                ");
+
+        System.out.println("Tamaño de la cola: " + cola.size());
+        System.out.println("               ");
+
+        //remueve
+        System.out.println("Remueve  el elemento: ");
+        System.out.println(cola.remove());
+        System.out.println("                ");
+
+        //limpiar la cola, borra todo de la cola
+        cola.clear();
+
+        System.out.println(cola.poll());//retorna null
+        System.out.println("             ");
+
 
     }
 }
