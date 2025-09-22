f3ebe1203741f9aa07f0806e6e92c4847db4281e
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-22 02:56:55 +0000

    creacion de la pila y mostrarla

--- MetodosVector.java/PruebaVectorAddIndex.java
+++ MetodosVector.java/PruebaVectorAddIndex.java
@@ -4,6 +4,12 @@ public class PruebaVectorAddIndex {
 
     public static void main(String[] args) {
         
-        
+        Stack<Integer> pila = new Stack<>();
+
+        pila.add(30);
+        pila.add(40);
+        pila.add(50);
+
+        System.out.println("Pila: " + pila);
     }
 }
