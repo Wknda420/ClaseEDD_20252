cb8f2c129825b41a3ce8bc26849b49beb349a8ec
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-22 05:58:55 +0000

    cracion de la pila

--- MetodosVector.java/PruebaContains.java
+++ MetodosVector.java/PruebaContains.java
@@ -2,6 +2,11 @@ import java.util.Stack;
 
 public class PruebaContains {
     public static void main(String[] args) {
-        
+        Stack<String> pila = new Stack<>();
+
+        pila.push("manzana");
+        pila.push("banana");
+        pila.push("naranja");
+        pila.push("uva");
     }
 }
