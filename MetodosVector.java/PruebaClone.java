6b97139d3eceb3af562663922c805c81c0d0c92d
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-22 05:39:27 +0000

    .

--- MetodosVector.java/PruebaClone.java
+++ MetodosVector.java/PruebaClone.java
@@ -8,6 +8,7 @@ public class PruebaClone {
         Original.push("Gato Persa");
         Original.push("Gato Carey");
 
+        System.out.println("                         ");
         System.out.println("Pila original: " + Original);
 
         Stack<String> clone = (Stack<String>) Original.clone();
@@ -15,8 +16,19 @@ public class PruebaClone {
         System.out.println("Copia: " + clone);
 
         //verificar contenido
+        System.out.println("----------------------------");
         System.out.println("¿Tienen el mismo contenido? " + Original.equals(clone));
-        
+        System.out.println("----------------------------");
+
+
+        //independencia
+        Original.push("Gato enano");
+
+        System.out.println("Original con una especie mas agregada: " + Original);
+        System.out.println("Copia sin cambios: " + clone);
+        System.out.println("----------------------------");
+        System.out.println("¿Tienen el mismo contenido? " + Original.equals(clone));
+
 
     }
 }
