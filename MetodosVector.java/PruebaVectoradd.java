47c45f7f61cb7790c8d7c299904409146127e50d
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-22 01:04:30 +0000

    .

--- MetodosVector.java/PruebaVectoradd.java
+++ MetodosVector.java/PruebaVectoradd.java
@@ -2,7 +2,13 @@ import java.util.Vector;
 
 public class PruebaVectoradd {
     public static void main(String[] args) {
-        
+        Vector<Integer> numeros = new Vector<>();
+
+        numeros.add(10);
+        numeros.add(20);
+        numeros.add(30);
+
+        System.out.println("Numeros: " + numeros);
     }
     
 }
