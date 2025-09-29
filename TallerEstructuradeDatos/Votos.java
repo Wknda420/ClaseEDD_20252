a605831603545d7674bb673072702774b3d3f7a1
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-29 06:38:21 +0000

    .

--- TallerEstructuradeDatos/Votos.java
+++ TallerEstructuradeDatos/Votos.java
@@ -1,5 +1,25 @@
 package TallerEstructuradeDatos;
 
+import java.util.Stack;
+
 public class Votos {
-    
+    public static int main(int afavor, int encontra) {
+        
+        Stack<Integer> pila = new Stack<>();
+
+        pila.push(afavor);
+        pila.push(encontra);
+
+        int contra = pila.pop();
+        int favor = pila.pop();
+
+        return afavor - encontra;
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println(" ");
+        System.out.println("Recuento de votos:");
+        System.out.println(main(20, 9));
+    }
 }
