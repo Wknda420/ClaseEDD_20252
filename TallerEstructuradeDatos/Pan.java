681233c1869b4054c7fbd0808174775187c48cd8
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-29 07:27:00 +0000

    llenar la pila

--- TallerEstructuradeDatos/Pan.java
+++ TallerEstructuradeDatos/Pan.java
@@ -7,6 +7,14 @@ public class Pan {
         Stack<String> pila1 = new Stack<>();
         Stack<String> pila2 = new Stack<>();
 
+        for (String ingredientes: sandwich1) {
+            pila1.push(ingredientes);
+        }
+
+        for (String ingredientes: sandwich2) {
+            pila2.push(ingredientes);
+        }
+
         
 
         return false;
