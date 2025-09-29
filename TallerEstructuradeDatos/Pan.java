dadfbd8aa89b775a7e5ebf0b6eab3bf9836fcef3
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-29 07:04:28 +0000

    ejercicio2

---
+++ TallerEstructuradeDatos/Pan.java
@@ -0,0 +1,11 @@
+package TallerEstructuradeDatos;
+
+import java.util.Stack;
+
+public class Pan {
+    public static void main(String[] args) {
+        Stack<Integer> pila = new Stack<>();
+
+        
+    }
+}
