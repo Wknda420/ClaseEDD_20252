7a0f76b5ebcfa73777c8311a5a8d905f0bd545d4
Wanda <wanda.montanobet@campusucc.edu.co>
2025-09-29 06:43:38 +0000

    main

--- TallerEstructuradeDatos/Votos.java
+++ TallerEstructuradeDatos/Votos.java
@@ -14,12 +14,20 @@ public class Votos {
         int favor = pila.pop();
 
         return afavor - encontra;
+
         
     }
 
     public static void main(String[] args) {
         System.out.println(" ");
-        System.out.println("Recuento de votos:");
+        System.out.println("Recuento de votos N°1:");
         System.out.println(main(20, 9));
+        System.out.println(" ");
+        System.out.println("Recuento de votos N°2:");
+        System.out.println(main(2, 33));
+        System.out.println(" ");
+        System.out.println("Recuento de votos N°3:");
+        System.out.println(main(20, 20));
+
     }
 }
