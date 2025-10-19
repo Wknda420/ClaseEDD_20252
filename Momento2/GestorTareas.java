97bc3ed8b78737f27b48941f404627e36898cbdb
Wanda <wanda.montanobet@campusucc.edu.co>
2025-10-19 19:27:00 +0000

    metodo para agregar una tarea al sistema

--- Momento2/GestorTareas.java
+++ Momento2/GestorTareas.java
@@ -4,4 +4,13 @@ public class GestorTareas {
     private Queue<Tarea> colaEspera = new LinkedList<>();
     private Stack<Tarea> pilaPrioritaria = new Stack<>();
     private Map<String, String> registroTrazabilidad = new HashMap<>();
+
+    public void agregarTarea(Tarea tarea){
+        if (tarea.getPrioridad() == 3){
+            pilaPrioritaria.push(tarea);
+        }
+        else{
+            colaEspera.add(tarea);
+        }
+    }
 }
