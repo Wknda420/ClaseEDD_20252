eb697ed8c636c59365a69762cc28920936e00de9
Wanda <wanda.montanobet@campusucc.edu.co>
2025-10-19 19:15:45 +0000

    constructor

--- Momento2/ClaseTarea.java
+++ Momento2/ClaseTarea.java
@@ -2,5 +2,12 @@ public class ClaseTarea {
     private String id;
     private String descripcion;
     private int prioridad;
-    private long tiempoLllegada;
+    private long tiempoLlegada;
+
+    public ClaseTarea(String id, String descripcion, int prioridad) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.tiempoLlegada = System.currentTimeMillis();
+    }
 }
