380662932bd7106f63c2ee89a252d4b6ef392488
Wanda <wanda.montanobet@campusucc.edu.co>
2025-10-19 19:58:33 +0000

    agregar al sistema

--- Momento2/Main.java
+++ Momento2/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         GestorTareas gestor = new GestorTareas();
 
-        Tarea normal = new Tarea("T1", "Tarea normal", 1);
-        Tarea urgente = new Tarea("T2", "Tarea urgente", 3);
-        Tarea media = new Tarea("T3", "Tarea media", 2);
+        ClaseTarea normal = new ClaseTarea("T1", "Tarea normal", 1);
+        ClaseTarea urgente = new ClaseTarea("T2", "Tarea urgente", 3);
+        ClaseTarea media = new ClaseTarea("T3", "Tarea media", 2);
+
+        gestor.agregarTarea(normal);
+        gestor.agregarTarea(urgente);
+        gestor.agregarTarea(media);
     }
 }
