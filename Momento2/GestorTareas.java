80d033fe5bb2823d7b22754c752d7a27d9574a27
Wanda <wanda.montanobet@campusucc.edu.co>
2025-10-19 19:23:30 +0000

    declarar estructuras

--- Momento2/GestorTareas.java
+++ Momento2/GestorTareas.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
 public class GestorTareas {
-    
+    private Queue<Tarea> colaEspera = new LinkedList<>();
+    private Stack<Tarea> pilaPrioritaria = new Stack<>();
+    private Map<String, String> registroTrazabilidad = new HashMap<>();
 }
