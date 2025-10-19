114869102eccf60e6d00e400e6e1871404a45ed0
Wanda <wanda.montanobet@campusucc.edu.co>
2025-10-19 19:56:59 +0000

    crear tareas con ejemplo de diferentes prioridades

--- Momento2/Main.java
+++ Momento2/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         GestorTareas gestor = new GestorTareas();
+
+        Tarea normal = new Tarea("T1", "Tarea normal", 1);
+        Tarea urgente = new Tarea("T2", "Tarea urgente", 3);
+        Tarea media = new Tarea("T3", "Tarea media", 2);
     }
 }
