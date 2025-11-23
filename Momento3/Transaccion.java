7d0917902b10d31fcc7a34f7129d09e84be771ff
Wanda <wanda.montanobet@campusucc.edu.co>
2025-11-23 23:54:12 +0000

    arreglo en transaccion

--- Momento3/Transaccion.java
+++ Momento3/Transaccion.java
@@ -6,14 +6,28 @@ public class Transaccion {
     String cuentaDestino;
     double monto;
 
+    String nombreOrigen;
+    String nombreDestino;
+
     public Transaccion(String tipo, String cuentaOrigen,String cuentaDestino, double monto) {
         this.tipo = tipo;
         this.cuentaOrigen = cuentaOrigen;
         this.cuentaDestino = cuentaDestino;
         this.monto = monto;
+        this.nombreOrigen = "";
+        this.nombreDestino = "";
 
     }
     public void mostrar() {
-        System.out.printf("%s | Monto: $%.2f | Origen: %s | Destino: %s", tipo, monto, cuentaOrigen, cuentaDestino != null? cuentaDestino: "N/A");
+        if (tipo.equals("TRANSFERENCIA")) {
+            System.out.printf("%s | Monto: $%.2f | Origen: %s (%s) | Destino: %s (%s)",
+                tipo, monto, cuentaOrigen, nombreOrigen, cuentaDestino, nombreDestino);
+        } else if (tipo.equals("DEPOSITO")) {
+            System.out.printf("%s | Monto: $%.2f | Cuenta: %s (%s)",
+                tipo, monto, cuentaOrigen, nombreOrigen);
+        } else { // RETIRO
+            System.out.printf("%s | Monto: $%.2f | Cuenta: %s (%s)",
+                tipo, monto, cuentaOrigen, nombreOrigen);
+        }
     }
 }
