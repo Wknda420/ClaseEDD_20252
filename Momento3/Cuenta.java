5aa90555a4dc8c2a13052f897330c934339eff9c
Wanda <wanda.montanobet@campusucc.edu.co>
2025-11-23 05:53:34 +0000

    cuenta bancaria

--- Momento3/Cuenta.java
+++ Momento3/Cuenta.java
@@ -1,5 +1,21 @@
 package Momento3;
 
 public class Cuenta {
-    
+    String numeroCuenta;
+    String titular;
+    double saldo;
+    String tipoCuenta;
+    boolean activa;
+
+    public Cuenta(String numeroCuenta, String titular, double saldoInicial, String tipoCuenta) {
+        this.numeroCuenta =numeroCuenta;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+        this.tipoCuenta = tipoCuenta;
+        this.activa = true;
+    }
+
+    public void mostrar() {
+        System.out.printf("Cuenta: %s | Titular: %s | Saldo: $%.2f | Tipo: %s\n", numeroCuenta,titular, saldo, tipoCuenta);
+    }
 }
