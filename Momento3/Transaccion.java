c83fe704d2febdaa7260386fcc4d4a9f082495f1
Wanda <wanda.montanobet@campusucc.edu.co>
2025-11-23 06:03:52 +0000

    representa una transaccion bancaria

--- Momento3/Transaccion.java
+++ Momento3/Transaccion.java
@@ -5,4 +5,15 @@ public class Transaccion {
     String cuentaOrigen;
     String cuentaDestino;
     double monto;
+
+    public Transaccion(String tipo, String cuentaOrigen,String cuentaDestino, double monto) {
+        this.tipo = tipo;
+        this.cuentaOrigen = cuentaOrigen;
+        this.cuentaDestino = cuentaDestino;
+        this.monto = monto;
+
+    }
+    public void mostrar() {
+        System.out.printf("%s | Monto: $%.2f | Origen: %s | Destino: %s\n", tipo, monto, cuentaOrigen, cuentaDestino != null? cuentaDestino: "N/A");
+    }
 }
