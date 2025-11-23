9d9c4da1c270442b97a1194cb822738bb213d4c5
Wanda <wanda.montanobet@campusucc.edu.co>
2025-11-23 21:32:02 +0000

    clase principal con menu

---
+++ Momento3/SistemaBancario.java
@@ -0,0 +1,9 @@
+package Momento3;
+import java.util.Scanner;
+
+public class SistemaBancario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GestorBancario gestor = new GestorBancario();
+    }
+}
