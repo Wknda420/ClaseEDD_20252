45a9b436e9c7fd6986d98561ec30212410aa12ef
Wanda <wanda.montanobet@campusucc.edu.co>
2025-11-23 07:20:51 +0000

    validacion de cuentas, actualizacion de titular, busqueda, eliminacion, returns

--- Momento3/GestorBancario.java
+++ Momento3/GestorBancario.java
@@ -17,6 +17,46 @@ public class GestorBancario {
     }
 
     public void crearCuenta(String numero, String titular, double saldo, String tipo ) {
-        
+        if (buscarCuenta(numero) != null) {
+            System.out.println("ERROR: La cuneta ya existe");
+            return;
+        }
+        if (saldo < 0) {
+            System.out.println("ERROR: El saldo no puede ser negativo");
+            return;
+        }
+        Cuenta nueva = new Cuenta(numero, titular, saldo, tipo);
+        cuentas.add(nueva);
+        System.out.println("Cuenta creada de manera exitosa");
+    }
+    public Cuenta buscarCuenta(String numero) {
+        for (Cuenta c: cuentas) {
+            if (c.numeroCuenta.equals(numero)&&c.activa) {
+                return c;
+            }
+        }
+        return null;
+    }
+    public void actualizarCuenta(String numero, String nuevoTitular) {
+        Cuenta cuenta = buscarCuenta(numero);
+        if (cuenta == null) {
+            System.out.println("Cuenta no encontrada :(");
+            return;
+        }
+        cuenta.titular = nuevoTitular;
+        System.out.println("La cuenta ha sido actualizada");
+    }
+    public void eliminarCuenta(String numero) {
+        Cuenta cuenta = buscarCuenta(numero);
+        if (cuenta == null) {
+            System.out.println("La cuenta no ha sido encontrada");
+            return;
+        }
+        if (cuenta.saldo > 0) {
+            System.out.println("No se puede eliminar una cuenta con saldo");
+            return;
+        }
+        cuenta.activa = false;
+        System.out.println("Cuenta eliminada");
     }
 }
